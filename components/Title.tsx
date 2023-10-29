34a8a232f7cbda76cd28077d9d488251a8a03d46
alexandrexyz <costaalexandre45@gmail.com>
2023-10-29 20:54:08 -0300

    Fix renderer problem with marginButton

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,13 +2,12 @@ import { FC, HTMLAttributes } from "react";
 
 interface ITitle extends HTMLAttributes<HTMLHeadingElement> {
     children: JSX.Element | string;
-    marginButtom?: number;
 }
 
-export const Title: FC<ITitle> = ({ children, className, marginButtom = 4,...props }) => {
+export const Title: FC<ITitle> = ({ children, className, ...props }) => {
   return (
     <h2
-      className={`text-primary font-medium text-3xl mb-${marginButtom} text-left ${className ?? ''}`}
+      className={`text-primary font-medium text-3xl mb-4 text-left ${className ?? ''}`}
       {...props}
     >
       {children}
