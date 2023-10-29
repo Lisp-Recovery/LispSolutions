b3c6e4f61496152d2538f166ddad1c7660b5b1cb
alexandrexyz <costaalexandre45@gmail.com>
2023-10-29 18:19:13 -0300

    Fix marginButton in Title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -5,7 +5,7 @@ interface ITitle extends HTMLAttributes<HTMLHeadingElement> {
     marginButtom?: number;
 }
 
-export const Title: FC<ITitle> = ({ children, className, marginButtom = '4',...props }) => {
+export const Title: FC<ITitle> = ({ children, className, marginButtom = 4,...props }) => {
   return (
     <h2
       className={`text-primary font-medium text-3xl mb-${marginButtom} text-left ${className}`}
