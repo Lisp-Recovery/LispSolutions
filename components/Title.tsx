859c8d89e5621bb19c216f4ba6922d3621bb0872
alexandrexyz <costaalexandre45@gmail.com>
2023-10-29 19:56:30 -0300

    Fix Title className

--- components/Title.tsx
+++ components/Title.tsx
@@ -8,7 +8,7 @@ interface ITitle extends HTMLAttributes<HTMLHeadingElement> {
 export const Title: FC<ITitle> = ({ children, className, marginButtom = 4,...props }) => {
   return (
     <h2
-      className={`text-primary font-medium text-3xl mb-${marginButtom} text-left ${className}`}
+      className={`text-primary font-medium text-3xl mb-${marginButtom} text-left ${className ?? ''}`}
       {...props}
     >
       {children}
